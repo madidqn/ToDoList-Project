3cb36d9b5a6d77b4e3f028aacffacff915d284dd
mahdieh <madidgn82@gmail.com>
2024-04-25 18:10:33 +0330

    create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+const inputUser = document.querySelector("#userInput");
+const btnSubmit = document.querySelector("#submit");
+const ulToDoList = document.querySelector(".boxToDo");
+const ulToDoDone = document.querySelector("#toDoDone");
+const buttonElements = document.querySelector(".wrapperButtom");
+const btnEdit = document.querySelector("#edit");
